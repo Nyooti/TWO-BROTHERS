c4eabc99af880c61e6dbc051a97174b9bc13714d
Nyooti <nyootitom@gmail.com>
2025-07-06 10:51:22 +0300

    Deploy project

---
+++ configs.php
@@ -0,0 +1,3 @@
+<?php
+$SecretKey = "sk_live_a96238f356abe1ad14045c0cd068430b0424b72b"; // Add your secret key here. Remember to change this to your live secret key in production
+$PublicKey = "pk_live_703c7066f22b0a99ee244f1cad6343ba906cc2cc"; // Add your public key here. Remember to change this to your live public key in production
